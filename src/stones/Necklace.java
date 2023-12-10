6a854fa0186fce4a02d47dc3465f3940b958d36d
Alex Shopiak <alshop2004@gmail.com>
2023-12-10 17:20:43 +0000

    Implement Necklace class

--- src/stones/Necklace.java
+++ src/stones/Necklace.java
@@ -9,15 +9,48 @@ public class Necklace{
         necklace = new ArrayList<>();
     }
 
-    public void addPearl(int weight) {
-        necklace.add(new Pearl(weight));
-    }//todo example
-
-    public int getWeightInCarats() {
-        return 0;//todo
+    public void addStone(String name, int weight) {
+        switch (name.toLowerCase()) {
+            case "diamond":
+                necklace.add(new Diamond(weight));
+                break;
+            case "emerald":
+                necklace.add(new Emerald(weight));
+                break;
+            case "ruby":
+                necklace.add(new Ruby(weight));
+                break;
+            case "onyx":
+                necklace.add(new Onyx(weight));
+                break;
+            case "opal":
+                necklace.add(new Opal(weight));
+                break;
+            case "pearl":
+                necklace.add(new Pearl(weight));
+                break;
+            default:
+                throw new IllegalArgumentException("We don't have " + name);
+        } 
     }
 
-    public int getPriceInUSD() {
-        return 0;//todo
+    public int getWeight() {
+        int weight = 0;
+
+        for (Stone stone : necklace) {
+            weight += stone.getWeight();
+        }
+
+        return weight;
+    }
+
+    public int getPrice() {
+        int price = 0;
+
+        for (Stone stone : necklace) {
+            price += stone.getWeight() * stone.getPrice();
+        }
+
+        return price;
     }
 }

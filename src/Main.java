eaf1956422279b63abf959b1196e64ab3122967e
Alex Shopiak <alshop2004@gmail.com>
2023-12-10 17:21:37 +0000

    Update MAin according to new contract

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Necklace necklace = new Necklace();
 
-        necklace.addPearl(2);
-        necklace.addPearl(1);
+        necklace.addStone("Diamond", 2);
+        necklace.addStone("Pearl", 2);
 
-        int result = necklace.getSize();
-        System.out.println(result);
+        int weight = necklace.getWeight();
+        int price = necklace.getPrice();
+
+        System.out.println("Weight: " + weight + " carats");
+        System.out.println("Price : " + price + " USD");
     }
 }

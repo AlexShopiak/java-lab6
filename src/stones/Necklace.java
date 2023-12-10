9af5836c5a993e4bfaab7524a1113aa9108dd669
Alex Shopiak <alshop2004@gmail.com>
2023-12-10 20:49:48 +0000

    Add filter method

--- src/stones/Necklace.java
+++ src/stones/Necklace.java
@@ -2,8 +2,12 @@ package stones;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Iterator;
 
 public class Necklace{
+    private static final int MIN_CLR = 1;
+    private static final int MAX_CLR = 10;
+
     private ArrayList<Stone> necklace;
 
     public Necklace() {
@@ -46,7 +50,18 @@ public class Necklace{
         }
     }
 
-    public void filterByTransparency() { 
+    public void filterByClearence(int bottom, int top) {
+        clarityValidator(bottom);
+        clarityValidator(top);
+        Iterator<Stone> iterator = necklace.iterator();
+
+        while (iterator.hasNext()) {
+            Stone stone = iterator.next();
+            int clarity = stone.getClarity();
+            if (clarity < bottom || clarity > top) {
+                iterator.remove();
+            }
+        }
     }
 
     public int getWeight() {
@@ -72,8 +87,8 @@ public class Necklace{
     public void display(String title) {
         String BOLD_STR = "\u001B[1m";
         String BOLD_END = "\u001B[0m";
-
         System.out.println(BOLD_STR + title + BOLD_END);
+
         for (Stone stone : necklace) {
             String name = stone.getClass().getSimpleName();
             int weight = stone.getWeight();
@@ -84,5 +99,11 @@ public class Necklace{
         System.out.println(BOLD_STR + "Weight: " + BOLD_END + getWeight() + " carats");
         System.out.println(BOLD_STR + "Price : " + BOLD_END +  getPrice() + " USD");
         System.out.println();
+    }
+
+    private void clarityValidator(int clarity) {
+        if (clarity < MIN_CLR || clarity > MAX_CLR ) {
+            throw new IllegalArgumentException("Clarity must be 1-10");
+        }
     }
 }

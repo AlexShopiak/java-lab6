8ff2bba60333fee6e4b33ac110f576121415a02e
Alex Shopiak <alshop2004@gmail.com>
2023-12-11 12:26:33 +0000

    Upgrade Main use case

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,13 @@ import stones.Necklace;
 public class Main {
     public static void main(String[] args) {
         Necklace necklace = new Necklace();
-
+        
         necklace.addStone("Diamond", 2, 10);
-        necklace.addStone("Pearl", 2, 9);
+        necklace.addStone("Pearl",   2, 9);
         necklace.addStone("Emerald", 1, 1);
+        necklace.addStone("Emerald", 1, 1);
+        necklace.addStone("Emerald", 1, 1);
+        necklace.addStone("Ruby",    3, 7);
         necklace.display("Initial");
 
         necklace.sortByPrice();
